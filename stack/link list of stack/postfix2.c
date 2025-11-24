df80a6eb810d8e344ae461b580beffb3d5d9a252
rockabhishekak <abhishek.kumar20248@lpu.in>
2025-11-24 10:12:22 +0530

    Implement postfix expression evaluation and add error handling for invalid operators

--- stack/link list of stack/postfix2.c
+++ stack/link list of stack/postfix2.c
@@ -1,9 +1,38 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<ctype.h>
-#include<string.h>
 int main()
 {
-    
+    char expr[100];
+    int stack[100], top = -1;
+    printf("Enter a postfix expression: ");
+    scanf("%s", expr);
+    for(int i = 0; expr[i] != '\0'; i++)
+    {
+        char ch = expr[i];
+        if(isdigit(ch))
+        {
+            stack[++top] = ch - '0';
+        }
+        else
+        {
+            int val2 = stack[top--]; //pop()
+            int val1 = stack[top--]; //pop()
+            int result;
+            switch(ch)
+            {
+                case '+': result = val1 + val2; break;
+                case '-': result = val1 - val2; break;
+                case '*': result = val1 * val2; break;
+                case '/': result = val1 / val2; break;
+                default: 
+                    printf("Invalid operator: %c\n", ch);
+                    return 1;
+            }
+            stack[++top] = result; //push(result)
+            
+        }
+    }
+    printf("The result of the postfix expression is: %d\n",stack[top]);
+    return 0;
 }
     
