774f2027226660625c8bf73162fba4e687f44760
rockabhishekak <abhishek.kumar20248@lpu.in>
2025-12-11 22:03:22 +0530

    binaray search tree

---
+++ Binary_Search_Tree/BSTT.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* insert(Node* root, int val) {
+    Node* nn = new Node();
+    nn->data = val;
+    nn->left = nullptr;
+    nn->right = nullptr;
+    if (root == nullptr) {
+        return nn;
+    }
+    else{
+        if(val < root->data){
+            root->left = insert(root->left, val);
+        }
+        else{
+            root->right = insert(root->right, val);
+        }
+    }
+    
+    return root;
+}
+void inorder(Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    cout << "Inorder traversal of the BST: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
